3bcd10b175a5bcf49e30a640fd8ff556e9e23f59
李成刚 <lichenggang@uniontech.com>
2024-01-06 23:21:00 +0800

    feat: update frozen to 1.1.1

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/frozen/issues
+# Bug-Submit: https://github.com/<user>/frozen/issues/new
+# Changelog: https://github.com/<user>/frozen/blob/master/CHANGES
+# Documentation: https://github.com/<user>/frozen/wiki
+# Repository-Browse: https://github.com/<user>/frozen
+# Repository: https://github.com/<user>/frozen.git
